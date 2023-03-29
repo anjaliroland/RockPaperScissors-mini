89e318f04f849a44f4c889bd2a21abbe757a6048
Anjali Roland <anjaligroland@gmail.com>
2023-03-29 10:21:40 -0700

    added comments of TODO s

---
+++ script.js
@@ -0,0 +1,23 @@
+// TODO: collect and store user input
+    // TODO: use popup window to allow user to pick R, P, or S
+    // TODO: store result in a variable called userChoice
+    // TODO: EDGE CASE: what happens if the user picks another letter
+    // TODO: EDGE CASE: what happens if they use a lowercase variable
+
+// TODO: randomly let computer pick and store in variable
+    // TODO: possible choices inclue R, P, or S
+    // TODO: store in variable called compChoice
+
+// TODO: compare user and computer choice
+// TODO: display results of game
+    // TODO: if user = rock and comp = scissors or user = scissors and comp = paper or user = paper and comp = rock, user wins
+    // TODO: if user = rock and comp = paper or user = scissors and comp = rock or user = paper and comp = scissors, comp wins
+    // TODO: if user = rock and comp = rock or user = scissors and comp = scissors or user = paper and comp = paper, tie
+
+// TODO: display stats
+    // TODO: track wins, losses and ties
+    // TODO: display all three in one screen
+
+// TODO: ask user if they want to play again
+    // TODO: if they say yes, restart whole game
+    // TODO: if they say no, stop doing anything
